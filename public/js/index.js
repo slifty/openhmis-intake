121f3d4f827f82c9bc244c33d000453169deeec1
noeltaylor <noeltaylo@gmail.com>
2015-07-24 03:11:41 -0500

    Continue work on UI demo
    Allow addtion of new clients even when search returns results, but also
    display a warning when adding a new client when results are present. The
    warning does not display when no search results are returned. The "add
    new client" button continues to be disabled until at least a minimum
    number of characters is entered.
    
    Select boxes on the intake form (gender, ethnicity, and race) now
    reflect the values of the selected entity, and their values are saved to
    that entity if changed by the user.

--- public/js/index.js
+++ public/js/index.js
@@ -12,25 +12,40 @@ $(function() {
     });
 
     // event handlers
-    $("#searchField").keyup(function() {
-      var userString = $("#searchField").val();
+    $("#searchForm #searchField").keyup(function() {
+      var userString = $("#searchForm #searchField").val();
       if (userString.length >= minSearchLength) {
-        populateResults(userString);
+        var numResults = populateResults(userString);
+        // If "results" is empty, activate the "add new client" button.
+        if (numResults > 0) {
+          // We've found some results, but we may still have to add a
+          // new client (maybe a person with the same name as an
+          // existing client). The "add new client" button will be
+          // activated, but add a warning too.
+          $("#searchForm #warningMessage").css("display", "block");
+        }
+        else {
+          $("#searchForm #warningMessage").css("display", "none");
+          // No need for the warning.
+        }
+        // If we're over the minimum length, we may add a new client.
+        $("#searchForm #addNewClient").prop("disabled", false);
       }
       else if (userString.length == 0) {
-        $("#results").empty(); // seems like this should have happened anyway, but it wasn't; look into it; also might want to shorten this comment.
+        $("#searchForm #results").empty();
+        $("#searchForm #warningMessage").css("display", "none");
       }
     });
-    $("#results").on("click", ".hit", function(e) {
+    $("#searchForm #results").on("click", ".hit", function(e) {
       switchToIntake($(e.currentTarget).data("entity-index"));
     });
-    $("#addNewClient").click(function() {
+    $("#searchForm #addNewClient").click(function() {
       switchToIntake(-1);
     });
-    $("#backToResults").click(function() {
+    $("#intakeForm #backToResults").click(function() {
       switchToSearch(true);
     });
-    $("#saveChanges").click(function() {
+    $("#intakeForm #saveChanges").click(function() {
       saveChanges();
       switchToSearch(false);
     });
@@ -46,7 +61,7 @@ $(function() {
     for (var i=0; i<newHits.length; i++) {
       newHitIndices.push(newHits[i].entityIndex);
     }
-    var oldHits = $("#results .hit");
+    var oldHits = $("#searchForm #results .hit");
     oldHits.each(function() {
       // Remember these are not objects of class Hit;
       // they're DOM elements (of class "hit").
@@ -69,24 +84,14 @@ $(function() {
       }
     });
     // Now remove from the "results" div...
-    $("#results .hit.removeMe").remove();
+    $("#searchForm #results .hit.removeMe").remove();
     // And add all newHits...
     for (var i=0; i<newHits.length; i++) {
-      $("#results").append(getSummaryDiv(newHits[i]));
+      $("#searchForm #results").append(getSummaryDiv(newHits[i]));
     }
-    // At this point, if "results" is empty, activate the "add new
-    // client" button.
-    if ($("#results > .hit").length == 0) {
-      $("#addNewClient").prop("disabled", false);
-    }
-    // Otherwise deactivate it. This ensures that we can only add a
-    // client when we're sure (s)he isn't already in the database.
-    else {
-      $("#addNewClient").prop("disabled", true);
-    }
-  }
 
-  function Entity() {
+    // Return the number of matching entities.
+    return $("#searchForm #results > .hit").length;
   }
 
   function Hit(entity) {
@@ -95,9 +100,9 @@ $(function() {
     this.picture = entity.picture;
     this.firstName = entity.firstName;
     this.lastName = entity.lastName;
-    this.sex = entity.sex.substr(0,1).toUpperCase();
-    this.DOB = getFormattedDOB(entity.DOB);
-    this.age = getYearsOld(entity.DOB);
+    this.gender = entity.gender ? entity.gender.substr(0,1).toUpperCase() : "";
+    this.DOB = entity.DOB ? getFormattedDOB(entity.DOB) : "";
+    this.age = entity.DOB ? getYearsOld(entity.DOB) : "";
   }
 
   function getFormattedDOB(date) {
@@ -221,13 +226,13 @@ $(function() {
     var picture = $("<div class='picture'><img src=\"img/" + hit.picture + "\"></div>");
     var text = $("<div class='text'></div>");
     var fullName = $("<div class='summaryElement'><span>" + hit.firstName + " " + hit.lastName + "</span></div>");
-    var sex = $("<div class='summaryElement'><span>(" + hit.sex + ")</span></div>");
+    var gender = $("<div class='summaryElement'><span>(" + hit.gender + ")</span></div>");
     var clear = $("<div class='clear'></div>");
     var dob = $("<div class='summaryElement'><span class='label'>DOB: </span><span>" + hit.DOB + "</span></div>");
     var age = $("<div class='summaryElement'><span class='label'>age: </span><span>" + hit.age + "</span></div>");
     summaryDiv.append(picture);
     text.append(fullName);
-    text.append(sex);
+    text.append(gender);
     text.append(clear);
     text.append(dob);
     text.append(age);
@@ -239,10 +244,11 @@ $(function() {
   function switchToSearch(keepResults) {
     keepResults = keepResults == undefined ? false : keepResults;
     if (keepResults !== true) {
-      $("#searchField").val("");
-      $("#results").empty();
+      $("#searchForm #searchField").val("");
+      $("#searchForm #results").empty();
+      $("#searchForm #warningMessage").css("display", "none");
     }
-    $("#addNewClient").prop("disabled", true);
+    $("#searchForm #addNewClient").prop("disabled", true);
     $("#search").css("display", "block");
     $("#intake").css("display", "none");
   }
@@ -254,13 +260,15 @@ $(function() {
     var sampleDataLength = sampleData.length;
     var entity = null;
     if (entityIndex < 0) { // New client
-      entity = new Entity();
+      entity = {};
       entity.index = sampleDataLength;
       entity.picture = "unknown.png";
       entity.firstName = $("#searchForm #firstName").val();
       entity.lastName = $("#searchForm #lastName").val();
       entity.DOB = "";
-      entity.sex = "female"; // hard code this for now to prevent an error on reading
+      entity.gender = "";
+      entity.ethnicity = "";
+      entity.race = "";
       sampleData.push(entity);
     }
     else {
@@ -280,15 +288,25 @@ $(function() {
     // Fill in the readonly DOB and age
     refreshIntakeFormDOB(entity.DOB);
 
-    // Fill in the remainign text fields
+    // Fill in inputs fields
     for (prop in entity) {
-      elem = $("#intakeForm #"+prop);
-      if (elem !== null) {
-        if (elem.is("input")) {
-          elem.val(entity[prop]);
+      if (entity[prop] !== null) {
+        elem = $("#intakeForm #"+prop);
+        if (elem !== null) {
+          if (elem.is("input")) {
+            elem.val(entity[prop]);
+          }
+          else if (elem.is("select")) {
+            elem.children("option").each(function() {
+              if($(this).val().toLowerCase() == entity[prop].toLowerCase()) {
+                $(this).attr("selected", true);
+              }
+            });
+          }
         }
       }
     }
+
     $("#search").css("display", "none");
     $("#intake").css("display", "block");
   }
@@ -311,7 +329,7 @@ $(function() {
   }
 
   function saveChanges() {
-    var propertyList = ["firstName", "lastName", "SSN"];
+    var propertyList = ["firstName", "lastName", "SSN", "gender", "ethnicity", "race"];
     var propertyListLength = propertyList.length;
 
     // Assign the values to the entity that have been entered in input
